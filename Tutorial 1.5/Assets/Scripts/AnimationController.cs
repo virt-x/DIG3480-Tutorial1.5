33b00fa5544835026239d5db2b290f627d2fba23
virt-x <xvirt@outlook.com>
2020-02-15 07:39:06 -0500

    tutorial 1.5

---
+++ Tutorial 1.5/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetInteger("State", 1);
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            animator.SetInteger("State", 0);
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            animator.SetInteger("State", 2);
+        }
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            animator.SetInteger("State", 0);
+        }
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ Tutorial 1.5/Assets/Scripts/SoundController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour
+{
+    public AudioSource player;
+    public AudioClip sound1, sound2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            player.clip = sound1;
+            player.Play();
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            player.Stop();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            player.clip = sound2;
+            player.Play();
+        }
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            player.Stop();
+        }
+    }
+}
